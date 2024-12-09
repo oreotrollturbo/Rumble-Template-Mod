5138702722190dcc51d4ba32a59b0aecb54c6c8a
oreotrollturbo <oreotrollturbo@gmail.com>
2024-12-09 15:00:09 +0100

    Add project files.

---
+++ TestMod/Class1.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using BuildInfo = TestMod.BuildInfo;
+using RumbleModdingAPI;
+
+[assembly: MelonInfo(typeof(TestMod.Class1), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.Author)]
+[assembly: MelonGame(null, null)]
+
+namespace TestMod
+{
+    public static class BuildInfo
+    {
+        public const string ModName = "Test mod";
+        public const string ModVersion = "1.0";
+        public const string Description = "God knows";
+        public const string Author = "oreotrollutrbo";
+        public const string Company = "Crusalis LLC";
+    }
+
+    public class Class1 : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+
+        }
+    }
+}
